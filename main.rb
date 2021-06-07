ce92a2770c36fb264342c107a0d5766eebe5cdc1
Zayne Mason <zaynepop@pop-os.localdomain>
2021-06-06 23:52:12 -0700

    Adding files

---
+++ main.rb
@@ -0,0 +1,25 @@
+require "discordrb"
+config = File.foreach("config.txt").map {|line| line.split(' ').join(' ')}
+token = config[0].to_s
+bot = Discordrb::Commands::CommandBot.new token: "#{token}", client_id: "#{config[1].to_s}" , prefix: "dr! " , ignore_bots: true
+blacklist = File.foreach("blacklist.txt").map {|line| line.split(' ').join(' ')}
+
+bot.run true
+
+bot.ready do |event|
+  puts "Logged in as #{bot.profile.username} (ID:#{bot.profile.id}) | #{bot.servers.size} servers"
+end
+
+bot.command :testing do |event|
+  event.respond "I'm working. #{event.user.mention}"
+end
+
+bot.message do |message|
+  if blacklist.include?(message.content.downcase.delete(' '))
+    message.respond "You're weird #{message.user.mention}"
+    message.respond "Check this out later #{message.server.roles[2].mention}"
+  end
+end
+
+
+bot.run
