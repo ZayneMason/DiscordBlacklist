0b7d1d272da53978e9fdbe60aacad52afcc7f8d6
ZayneMason <83897407+ZayneMason@users.noreply.github.com>
2021-06-12 22:35:40 -0700

    Cleanup

--- main.rb
+++ main.rb
@@ -4,8 +4,6 @@ token = config[0].to_s
 bot = Discordrb::Commands::CommandBot.new token: "#{token}", client_id: "#{config[1].to_s}" , prefix: "dr! " , ignore_bots: true
 blacklist = File.foreach("blacklist.txt").map {|line| line.split(' ').join(' ')}
 
-
-
 bot.ready do |event|
   puts "Logged in as #{bot.profile.username} (ID:#{bot.profile.id}) | #{bot.servers.size} servers"
 end
